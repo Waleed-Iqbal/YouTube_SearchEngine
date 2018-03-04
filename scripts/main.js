a8f790b5e54aadf187ce704af36f1254131b0d19
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-03-04 14:34:41 +0500

    Search bar animations now working.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,37 @@
 ﻿
+
+// Project id ... youtube-searchengine-197007
+// Key ... AIzaSyCkVzJuXpfwuWlPj8gk8BnmszFAiNqJad8
+
+
+
+$(function () {
+    var searchField = $("#query");
+    var icon = $("#search-btn");
+
+    $(searchField).on("focus", function () {
+        $(this).animate({
+            width: "100%"
+        }, 400);
+
+        $(icon).animate({
+            right: "10px"
+        }, 400);
+    });
+
+    $(searchField).on("blur", function () {
+        if (searchField.val() === "") {
+            $(searchField).animate({
+                width: "45%"
+            }, 400, function () { });
+
+            $(icon).animate({
+                right: "360px"
+            }, 400, function () { });
+        }
+
+    });
+
+
+
+})
