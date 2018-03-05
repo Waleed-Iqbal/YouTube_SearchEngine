970d352ac6fd139b3d236830757b845798277b64
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-03-05 21:46:34 +0500

    gitignored updated and making video results containers

--- scripts/main.js
+++ scripts/main.js
@@ -32,14 +32,64 @@ $(function () {
             }, 400);
         }
     });
+
+    $("#search-form").submit(function (e) {
+        e.preventDefault();
+    });
 })
 
 
-function search() {
+function searchYouTube() {
     //clear results
     $("#results").html("");
     $("#buttons").html("");
 
     //Get form input
     var q = $("#query").val();
+
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: "snippet, id",
+            q: q,
+            type: "video",
+            key: "AIzaSyCkVzJuXpfwuWlPj8gk8BnmszFAiNqJad8"
+        },
+        function (response) {
+            var nextPageToken = response.nextPageToken;
+            var prevPageToken = response.prevPageToken;
+            console.log(response);
+
+            $.each(response.items, function (index, item) {
+                var output = getOutput(item);
+                $("#results").append(output);
+            });
+        });
+}
+
+function getOutput(item) {
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var date = item.snippet.publishedAt;
+
+    var output = "<li>" +
+        "<div class='list-left'>" +
+        "   <img src='" + thumb + "'>" +
+        "</div>" +
+        "<div class='list-right'>" +
+        "<h3>" + title + "</h3>" +
+        "<small>By <span class='cTitle'>" + channelTitle + "</span> on " + date + "</small>" +
+        "" +
+        "" +
+        "" +
+        "</div>"
+    "</li>";
+
+
+
+    return output;
+
+
 }
